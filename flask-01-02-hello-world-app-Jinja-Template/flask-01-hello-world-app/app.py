c0911c2047a07403490caf131efef5419019fe40
tburakd <tburakd@gmail.com>
2022-02-01 13:14:48 -0500

    Flask first session added

--- flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/app.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/app.py
@@ -4,20 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def head():
-    return "<h1>You are gonna be a good DevOps and AWS Solution Architech</h1>"
+    return "<h1>Hello World!</h1"
 
 @app.route("/second")
 def second():
     return "This is the second page"
 
-@app.route("/third/tunc")
+@app.route("/third/burak")
 def third():
-    return "This is the subpath of third path"
+    return "This is the subpath of the third path"
 
-@app.route("/forth/<string:id>")
-def forth(id):
+@app.route("/fourt/<string:id>")
+def fourt(id):
     return f'Id of this page is {id}'
 
-if __name__=="__main__":
+if __name__=="__main__":                    #if this is my main app then run it on debug mode(True)
     app.run(debug=True)
-    
--- flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/app.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/app.py
@@ -2,16 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def head():
-    return render_template("index.html", number1 = 34, number2 = 45)
+    return render_template("index.html", number1=34, number2=45)
+    
 
 @app.route("/function")
 def function():
     variable1 = 12
-    variable2 = 5
-    return render_template("body.html", num1 = variable1, num2= variable2, multp = variable1 * variable2)
+    variable2 = 4
+    return render_template("body.html", num1 = variable1, num2 = variable2, multp = variable1 * variable2)
+
+
+
+
+
+
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(debug=True)
